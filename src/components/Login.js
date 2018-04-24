6281250500347aa3911a8c1d323839b1cf118d04
Chris <cstagger@asu.edu>
2018-04-23 22:20:47 -0700

    css changes to login and header

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import Content from "./VoterComponents/Content";
 import Organizer from "../components/OrganizerComponents/Organizer"
 import * as Servers from './settings'
 import Cookies from 'js-cookie';
+import 'font-awesome/css/font-awesome.min.css';
 
 import { BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 
@@ -84,7 +85,7 @@ class Login extends React.Component {
       password: password
     };
     this.setState({
-      loading:  <div><i className="fas fa-spinner fa-spin " style={ {'font-size':'3em'} } ></i></div>
+      loading:  <i className="fa fa-spinner fa-spin fa-3x"/>
     });
     fetch(url, {
       method: "POST",
@@ -130,7 +131,7 @@ class Login extends React.Component {
       account_type: accountType
     };
     this.setState({
-      loading: <div><i className="fas fa-spinner fa-spin " style={ {'font-size':'3em'} } ></i></div>
+      loading: <i className="fa fa-spinner fa-spin fa-3x"/>
     });
 
     fetch(url, {
@@ -164,11 +165,14 @@ class Login extends React.Component {
 
   renderLogin = function() {
     return (
-      <section className="hero is-fullheight">
+      <section className="hero is-info is-fullheight">
         <div className="hero-body">
           <div className="container has-text-centered">
             <div className="column is-4 is-offset-4">
-              <h3 className="title has-text-grey">Login</h3>
+              <span className="icon is-large is-info is-inverted">
+                  <i className="fa fa-check-square-o fa-3x" aria-hidden="true"/>
+              </span>
+                <h1 className="title is-1">BallotBlock</h1>
               <div className="box">
               {this.state.loading}
                 <form>
@@ -178,6 +182,8 @@ class Login extends React.Component {
                         className="input is-large"
                         ref="user"
                         placeholder="Username"
+                        id="one"
+                        onKeyPress={this.listenKeyPress}
                         autoFocus
                       />
                     </div>
@@ -196,22 +202,16 @@ class Login extends React.Component {
                   </div>
                   <div className="field" />
                   <a
-                    className="button is-block is-info is-large is-fullwidth"
-                    onClick={this.loginClick}
-                  >
-                    Login
+                    className="button is-info is-outlined is-medium is-fullwidth"
+                    onClick={this.loginClick}>Login
                   </a>
                   &nbsp;
-                  <p className="has-text-danger" id="error">
-                    {this.state.loginError}
-                  </p>
+                  <p className="has-text-danger" id="error">&nbsp;&nbsp;{this.state.loginError}</p>
+                    <a className="has-text-grey">Need an account? </a>
+                    <a onClick={this.signUp} className="has-text-info is-inverted">Sign Up</a>{" "}
+                    &nbsp;&nbsp;
                 </form>
               </div>
-              <p className="has-text-grey">
-                <a onClick={this.signUp}>Sign Up</a>{" "}
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <a>Forgot Password</a> &nbsp;&nbsp;
-              </p>
             </div>
           </div>
         </div>
@@ -221,11 +221,15 @@ class Login extends React.Component {
 
   renderSignUp = function() {
     return (
-      <section className="hero  is-fullheight">
+      <section className="hero is-info is-fullheight">
         <div className="hero-body">
           <div className="container has-text-centered">
-            <div className="column is-5 is-offset-4">
-              <h3 className="title has-text-grey">Create Account</h3>
+            <div className="column is-4 is-offset-4">
+              <span className="icon is-large">
+                <i className="fa fa-check-square-o fa-3x" aria-hidden="true"/>
+              </span>
+              <h1 className="title is-1">BallotBlock</h1>
+                <h2 className="subtitle">Create Account</h2>
               <div className="box">
               {this.state.loading}
                 <form>
@@ -261,6 +265,7 @@ class Login extends React.Component {
                   </div>
                   <div className="field">
                     <div className="control">
+                      <a className="has-text-grey">Select account type:</a>&nbsp;
                       <div className="select is-medium is-fullwidth">
                         <select ref={input => (this.accountType = input)}>
                           <option value="voter">Voter</option>
@@ -272,21 +277,14 @@ class Login extends React.Component {
                     </div>
                   </div>
                   <div className="field" />
-                  <a
-                    className="button is-block is-info is-large is-fullwidth"
-                    onClick={this.createAccountClick}
-                  >
-                    Create Account
-                  </a>
+                  <a className="button is-block is-info is-outlined is-medium is-fullwidth" onClick={this.createAccountClick}>Create Account</a>
                   &nbsp;
                   <p className="has-text-danger">{this.state.signUpError}</p>
+                    <a className="">Already have an account? </a>
+                    <a onClick={this.haveAccount} className="has-text-info is-inverted">Sign In</a>{" "}
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 </form>
               </div>
-              <p className="has-text-grey">
-                <a onClick={this.haveAccount}>Already Have Account</a>{" "}
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <a>Forgot Password</a> &nbsp;&nbsp;&nbsp;
-              </p>
             </div>
           </div>
         </div>
